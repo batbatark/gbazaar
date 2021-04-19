4a54af21f7c73a4f171f7e9328f25bcc945eaf53
Batbatark <batbatar.kh@gmail.com>
2021-04-19 17:50:26 +0800

    2 dahi commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import"../src/styles/index.css"
 const elvenShieldRecipe={
     leatherStrips: 2,
     ironIngot: 1,
     refinedMoonstone: 4
 }
+const elvenGauntletsRecipe={
+    ...elvenShieldRecipe,
+    leather:1,
+    refinedMoonstone: 4,
+}
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
